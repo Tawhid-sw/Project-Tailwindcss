aa43a6e422ca7b6988e3d6889de23311964b249c
Tawhid-sw <tawhidsarkar684@gmail.com>
2024-07-29 18:41:17 +0600

    re-setup the tailwindcss Now Using online playCDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  // content: ['./index.html'],
   theme: {
-    extend: {},
-    fontFamily : {
+    extend: {
+      colors: {
+        'primary': '#3238f2'
+      },
+    fontFamily: {
       'display' : ["Quicksand", 'sans-serif'],
       'body' : ['Poppins' , 'sans-serif']
-    }
+    },
   },
+},
   plugins: [],
 }
 
